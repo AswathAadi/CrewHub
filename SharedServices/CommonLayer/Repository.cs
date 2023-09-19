19b5f0f31838b406c725bacd0ce9750b4c84c325
Aswathaman M <aswath.adit@gmail.com>
2023-09-19 13:54:06 +0530

    Base Setup File Changes for the Entity Framework Core.

---
+++ SharedServices/CommonLayer/Repository.cs
@@ -0,0 +1,18 @@
+﻿using SharedServices.CommonLayer.ICommonLayer;
+using SharedServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedServices.CommonLayer
+{
+    public class Repository<TEntity, TEntityKey> : IRepository<TEntity, TEntityKey> where TEntity : EntityBase<TEntityKey>
+
+    {
+        public void Dispose()
+        {            
+        }
+    }
+}
